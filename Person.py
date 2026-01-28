c8b6785d0e02c44ec32972fdd00422b528e0f3a1
Kosrtov_Artem <nagebator.koty@gmail.com>
2026-01-28 17:44:34 +0300

    class Person added

---
+++ Person.py
@@ -0,0 +1,12 @@
+class Persen:
+    name: str = ""
+    age: int = ""
+
+
+    def __init__(self, name: str):
+        self.name = name
+
+    # def __init__(self, name: str, age: float):
+    #     self.name = name
+    #     self.age = age
+
--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-# ghfgh
+from Person import *
+
+Person.person1 = Person("Hakan")
+
+print(Person.person1)
